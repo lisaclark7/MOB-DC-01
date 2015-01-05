bce6b6a45f9d2c7f899891d742a11fccf052c9ba
Lisa A Clark <lisaclark7@ymail.com>
2015-01-05 18:47:31 -0500

    Syncing up.

---
+++ Classwork/Storyboard Demo/Storyboard Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storyboard Demo
+//
+//  Created by Lisa A Clark on 12/22/14.
+//  Copyright (c) 2014 Lisa A Clark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
