bb2b912965e983428a78fce1426179a783d17bd1
Lisa A Clark <lisaclark7@ymail.com>
2015-01-26 18:09:18 -0500

    Peer Programming 1/2 assignment

---
+++ Homework/Peer Programming Assignment.playground/section-1.swift
@@ -0,0 +1,26 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+
+class Player {
+    var message: String = ""
+    var damage: Int = 0
+    var health = 100
+    var isAlive = true
+    
+        
+    func attack() (message: String, damage: Int) -> String {
+        return("I'm unstoppable.  \(self.damage)")
+    }
+    
+    func isAlive() 
+    }
+
+var GoodPlayer = Player()
+    
+
+
+var BadPlayer = Player()
+    
+
