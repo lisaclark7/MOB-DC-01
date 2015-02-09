b817d519f3a8c27b614624cd59cdf9b027d2e79b
Lisa A Clark <lisaclark7@ymail.com>
2015-02-09 18:26:21 -0500

    Classwork Feb 4

--- Classwork/Class Feb 4/Drawing In Code Part 1 Class Exercise/Drawing In Code Part 1 Class Exercise/ViewController.swift
+++ Classwork/Class Feb 4/Drawing In Code Part 1 Class Exercise/Drawing In Code Part 1 Class Exercise/ViewController.swift
@@ -15,35 +15,36 @@ class ViewController: UIViewController {
        
         var logoImage = UIImage(named: "XLME Logo.png")
         var logoImageView = UIImageView(image: logoImage)
-        logoImageView.frame = CGRect(x: 30, y: 30, width: logoImage!.size.width/1.5, height: logoImage!.size.width/1.5)
+        logoImageView.frame = CGRect(x: 85, y: 30, width: logoImage!.size.width/1.5, height: logoImage!.size.width/1.5)
         self.view.addSubview(logoImageView)
         
     
-        var title = UILabel(frame: CGRect(x: 30, y: 215, width: 100, height: 100))
+        var title = UILabel(frame: CGRect(x: 150, y: 215, width: 100, height: 100))
         title.text = "Log In"
+        title.textAlignment = .Center
         self.view.addSubview(title)
         
 
     
-        var userName = UILabel(frame: CGRect(x: 30, y: 300, width: 100, height: 30))
+        var userName = UILabel(frame: CGRect(x: 30, y: 403, width: 100, height: 30))
         userName.text = "Username"
         self.view.addSubview(userName)
         
-        var userNameField = UITextField(frame: CGRect(x: 30, y: 420, width: self.view.frame.width, height: 30))
+        var userNameField = UITextField(frame: CGRect(x: 30, y: 380, width: self.view.frame.width, height: 30))
         userNameField.backgroundColor = UIColor.orangeColor()   
         self.view.addSubview(userNameField)
         
         
-        var password = UILabel(frame: CGRect(x: 30, y: 350, width: 100, height: 30))
+        var password = UILabel(frame: CGRect(x: 30, y: 463, width: 100, height: 30))
         password.text = "Password"
         self.view.addSubview(password)
         
-        var passwordField = UITextField(frame: CGRect(x: 30, y: 380, width: self.view.frame.width, height: 30))
+        var passwordField = UITextField(frame: CGRect(x: 30, y: 440, width: self.view.frame.width, height: 30))
         passwordField.backgroundColor = UIColor.orangeColor()
         self.view.addSubview(passwordField)
         
         
-        var logInBtn = UIButton(frame: CGRect(x: 50, y: 450, width: self.view.frame.width, height: 30))
+        var logInBtn = UIButton(frame: CGRect(x: 50, y: 550, width: self.view.frame.width, height: 30))
         logInBtn.setTitle("Click Here", forState: .Normal)
         logInBtn.titleColorForState(.Normal)
         logInBtn.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
